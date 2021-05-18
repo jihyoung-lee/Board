91bf32fe4562947a7bb60c46d4b8b554b82ac556
J.H.Lee <67559886+jihyoung-lee@users.noreply.github.com>
2021-05-18 23:17:16 +0900

    Update views.py

--- board_app/views.py
+++ board_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .data import defCnt_list
 # Create your views here.
 from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    context = {'gubun_list': defCnt_list}
+    return render(request, 'list.html', context)
